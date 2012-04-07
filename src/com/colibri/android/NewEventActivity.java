49048ab7756244de05ff4de5ea93e80e5ca91168
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2012-04-07 16:04:36 +0300

    Added widget for selecting begin and end time.

--- src/com/colibri/android/NewEventActivity.java
+++ src/com/colibri/android/NewEventActivity.java
@@ -1,11 +1,75 @@
 package com.colibri.android;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.ptashek.widgets.datetimepicker.DateTimePicker;
 
 public class NewEventActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.newevent);
+		
+		this.findViewById(R.id.buttonStartTime);
+	}
+	
+	public void onButtonTimeClicked(View sender) {
+		showDateTimeDialogForButton((Button) sender);
+	}
+
+	private void showDateTimeDialogForButton(final Button button) {
+		final Dialog mDateTimeDialog = new Dialog(this);
+		final RelativeLayout mDateTimeDialogView = (RelativeLayout) getLayoutInflater().inflate(R.layout.date_time_dialog, null);
+		final DateTimePicker mDateTimePicker = (DateTimePicker) mDateTimeDialogView.findViewById(R.id.DateTimePicker);
+		
+		// Check is system is set to use 24h time (this doesn't seem to work as expected though)
+//		final String timeS = android.provider.Settings.System.getString(getContentResolver(), android.provider.Settings.System.TIME_12_24);
+		final boolean is24h = true;// !(timeS == null || timeS.equals("12"));
+		((Button) mDateTimeDialogView.findViewById(R.id.SetDateTime)).setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				mDateTimePicker.clearFocus();
+				String text = mDateTimePicker.get(Calendar.YEAR) + "/" + (mDateTimePicker.get(Calendar.MONTH)+1) + "/"
+						+ mDateTimePicker.get(Calendar.DAY_OF_MONTH);
+				button.setText(text + "  " + mDateTimePicker.get(Calendar.HOUR_OF_DAY) + ":" + mDateTimePicker.get(Calendar.MINUTE));
+				mDateTimeDialog.dismiss();
+			}
+		});
+
+		// Cancel the dialog when the "Cancel" button is clicked
+		((Button) mDateTimeDialogView.findViewById(R.id.CancelDialog)).setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mDateTimeDialog.cancel();
+			}
+		});
+
+		// Reset Date and Time pickers when the "Reset" button is clicked
+		((Button) mDateTimeDialogView.findViewById(R.id.ResetDateTime)).setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mDateTimePicker.reset();
+			}
+		});
+		
+		// Setup TimePicker
+		mDateTimePicker.setIs24HourView(is24h);
+		// No title on the dialog window
+		mDateTimeDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// Set the dialog content view
+		mDateTimeDialog.setContentView(mDateTimeDialogView);
+		// Display the dialog
+		mDateTimeDialog.show();
 	}
 }
